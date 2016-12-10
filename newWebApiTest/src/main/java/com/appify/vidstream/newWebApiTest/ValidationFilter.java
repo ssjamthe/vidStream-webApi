9ccb349beabed53511e2bd6e6b009e18445f66be
Swapnil Jamthe <swapnil@Swapnils-MacBook-Pro.local>
2016-12-10 10:11:24 +0530

    Removed commented code from ValidationFilter

--- newWebApiTest/src/main/java/com/appify/vidstream/newWebApiTest/ValidationFilter.java
+++ newWebApiTest/src/main/java/com/appify/vidstream/newWebApiTest/ValidationFilter.java
@@ -38,8 +38,7 @@ public class ValidationFilter implements Filter
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         String tokenHeader = httpRequest.getHeader("token");
         String appId = request.getParameter("appId");
-//        String tokenHeader = "78:82:AD:8B:F0:59:2B:E2:2C:AE:FC:1B:B4:5A:0F:24:69:4F:72:76";
-//        String appId = "15";
+        
 
         Map<String, AppInfo> appsInfoMap = new HashMap<String, AppInfo>();
         appsInfoMap = appDataLoader.getAppsData();
