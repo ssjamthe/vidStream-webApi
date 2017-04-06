883bd6b0f8a2ec9ccc5274006c77034b4ed1191a
kalyani <kalyani@192.168.0.102>
2017-04-06 19:24:43 +0530

    Fixed bug of properties not loading dynamically

--- appifyNewWebApi/src/main/java/com/appify/vidstream/newWebApi/data/CombinedPropertyDataLoader.java
+++ appifyNewWebApi/src/main/java/com/appify/vidstream/newWebApi/data/CombinedPropertyDataLoader.java
@@ -4,6 +4,8 @@ import com.appify.vidstream.newWebApi.data.jdbc.JDBCPropertyDataLoader;
 import com.google.common.util.concurrent.AbstractScheduledService;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -11,37 +13,47 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by swapnil on 01/12/16.
  */
+@Singleton
 public class CombinedPropertyDataLoader extends PropertyDataLoader {
 
-    private JDBCPropertyDataLoader jdbcPropertyDataLoader;
-    private FilePropertyDataLoader filePropertyDataLoader;
+	private JDBCPropertyDataLoader jdbcPropertyDataLoader;
+	private FilePropertyDataLoader filePropertyDataLoader;
+	private volatile Map<String, String> props = new HashMap<>();
 
+	@Inject
+	public CombinedPropertyDataLoader(JDBCPropertyDataLoader jdbcPropertyDataLoader,
+			FilePropertyDataLoader filePropertyDataLoader) {
 
-    @Inject
-    public CombinedPropertyDataLoader(JDBCPropertyDataLoader jdbcPropertyDataLoader,
-                                      FilePropertyDataLoader filePropertyDataLoader) {
+		this.jdbcPropertyDataLoader = jdbcPropertyDataLoader;
+		this.filePropertyDataLoader = filePropertyDataLoader;
 
-        this.jdbcPropertyDataLoader = jdbcPropertyDataLoader;
-        this.filePropertyDataLoader = filePropertyDataLoader;
+	}
 
-    }
+	@Override
+	public Map<String, String> getProps() {
+		return this.props;
+	}
 
-    @Override
-    public Map<String, String> getProps() {
-        Map<String, String> props = new HashMap<>();
-        //Database property takes precedence in case of clash.
-        props.putAll(filePropertyDataLoader.getProps());
-        props.putAll(jdbcPropertyDataLoader.getProps());
-        return props;
-    }
+	private void loadData() {
+		Map<String, String> props = new HashMap<>();
+		// Database property takes precedence in case of clash.
+		props.putAll(filePropertyDataLoader.getProps());
+		props.putAll(jdbcPropertyDataLoader.getProps());
+		this.props = props;
+	}
 
-    @Override
-    protected void work() {
+	@Override
+	protected void work() {
+		loadData();
+	}
 
-    }
+	@Override
+	protected void startUp() {
+		loadData();
+	}
 
-    @Override
-    protected Scheduler scheduler() {
-        return Scheduler.newFixedDelaySchedule(0, 5, TimeUnit.MINUTES);
-    }
+	@Override
+	protected Scheduler scheduler() {
+		return Scheduler.newFixedDelaySchedule(0, 5, TimeUnit.MINUTES);
+	}
 }
